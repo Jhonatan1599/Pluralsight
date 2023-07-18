3f49534d41dfcbc2b671dfbd06b2c5dd27e6a81e
Jhonatan <larico1599@gmail.com>
2023-07-18 14:38:50 -0500

    unit testing end section 3

---
+++ Unit Testing/Modulo 3/EmployeeManagement.Tests/EmployeeFactoryTests.cs
@@ -0,0 +1,108 @@
+﻿
+using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Tests
+{
+    public class EmployeeFactoryTests
+    {
+
+        // A unit test is automated test that tests a small piece of behavior, often simply testing the methods of a class. Improves application reliability at a much lower cost than manual testing
+
+
+        /*Naming Guidelines for Unit Tests
+         * CreateEmployee: A name for the unit that's being tested
+         * ContructInternalEmployee: The scenario which the unit is being tested
+         * SalaryMustBe2500: The expected behavior
+         */
+
+
+        /*
+         *Arrange: Setting up the test
+         * Act: Executing the actual test
+         * Assert: Verifying the executed action
+         */
+
+        [Fact]
+        public void CreateEmployee_ContructInternalEmployee_SalaryMustBe2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Docks");
+
+            Assert.Equal(2500, employee.Salary);
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.True(employee.Salary >= 3000 && employee.Salary <= 3500,
+                "Salary not in acceptable range.");
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.True(employee.Salary >= 2500);
+            Assert.True(employee.Salary <= 3500);
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.InRange(employee.Salary, 2500, 3500);
+        }
+
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            employee.Salary = 2500.123m;
+
+            // Assert
+            Assert.Equal(2500, employee.Salary, 0);
+        }
+
+        [Fact]
+        public void CreateEmployee_IsExternalIsTrue_ReturnTypeMustBeExternalEmployee()
+        {
+            // Arrange
+            var factory = new EmployeeFactory();
+
+            // Act
+            var employee = factory.CreateEmployee("Kevin", "Dockx", "Marvin", true);
+
+            // Assert
+            Assert.IsType<ExternalEmployee>(employee);
+            //Assert.IsAssignableFrom<Employee>(employee);
+        }
+    }
+
+
+}
