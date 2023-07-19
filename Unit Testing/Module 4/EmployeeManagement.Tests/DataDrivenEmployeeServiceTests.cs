b006e01125dc99d7be8bbb44b87013b254cc0129
Jhonatan <larico1599@gmail.com>
2023-07-19 07:49:32 -0500

    unit testing end section 4

---
+++ Unit Testing/Module 4/EmployeeManagement.Tests/DataDrivenEmployeeServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Tests
+{
+    [Collection("EmployeeServiceCollection")]
+    public class DataDrivenEmployeeServiceTests //: IClassFixture<EmployeeServiceFixture>
+    {
+        private readonly EmployeeServiceFixture _employeeServiceFixture;
+
+        public DataDrivenEmployeeServiceTests(
+            EmployeeServiceFixture employeeServiceFixture)
+        {
+            _employeeServiceFixture = employeeServiceFixture;
+        }
+
+        [Fact]
+        public async Task GiveRaise_MinimumRaiseGiven_EmployeeMinimumRaiseGivenMustBeTrue()
+        {
+            // Arrange  
+            var internalEmployee = new InternalEmployee(
+                "Brooklyn", "Cannon", 5, 3000, false, 1);
+
+            // Act
+            await _employeeServiceFixture
+                .EmployeeService.GiveRaiseAsync(internalEmployee, 100);
+
+            // Assert
+            Assert.True(internalEmployee.MinimumRaiseGiven);
+        }
+
+
+        [Fact]
+        public async Task GiveRaise_MoreThanMinimumRaiseGiven_EmployeeMinimumRaiseGivenMustBeFalse()
+        {
+            // Arrange  
+            var internalEmployee = new InternalEmployee(
+                "Brooklyn", "Cannon", 5, 3000, false, 1);
+
+            // Act 
+            await _employeeServiceFixture.EmployeeService
+                .GiveRaiseAsync(internalEmployee, 200);
+
+            // Assert
+            Assert.False(internalEmployee.MinimumRaiseGiven);
+        }
+    }
+}
---
+++ Unit Testing/Module 4/EmployeeManagement.Tests/Fixtures/EmployeeServiceCollectionFixture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagement.Tests.Fixtures;
+using Xunit;
+
+namespace EmployeeManagement.Test.Fixtures
+{
+    [CollectionDefinition("EmployeeServiceCollection")]
+    public class EmployeeServiceCollectionFixture 
+        : ICollectionFixture<EmployeeServiceFixture>
+    {
+    }
+}
